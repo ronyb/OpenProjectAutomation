a6376a717d97f8fb1952786657e6323361e3b617
Rony Byalsky <rony.byalsky@gmail.com>
2021-02-17 15:05:26 +0200

    First commit: test_001_view_work_package

---
+++ tests/api/test_002_edit_work_package.py
@@ -0,0 +1,4 @@
+# https://docs.openproject.org/api/endpoints/work-packages/#work-packages-work-package
+# PATCH /api/v3/work_packages/{id}{?notify}
+def test_002_edit_work_package():
+    pass
---
+++ tests/api/test_003_delete_work_package.py
@@ -0,0 +1,4 @@
+# https://docs.openproject.org/api/endpoints/work-packages/#work-packages-work-package
+# DELETE /api/v3/work_packages/{id}{?notify}
+def test_003_delete_work_package():
+    pass
